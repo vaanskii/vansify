c31c4815a87dc30646d22051cc70668837119e4a
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-10-17 20:09:33 +0400

    Add email verification process. - Integrated email service for sending verification links. - Created token generation for verification.

---
+++ utils/emailverification.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// GenerateVerificationToken generates a random verification token
+func GenerateVerificationToken(email string) string {
+	// Create a random byte slice
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+
+	return base64.URLEncoding.EncodeToString(b) + ":" + email 
+}
