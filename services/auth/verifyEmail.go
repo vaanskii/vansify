e147c988df1551a90f0958f8a4ec01a72d129262
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-10-17 21:19:48 +0400

    change handlers structure to services/auth.

---
+++ services/auth/verifyEmail.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vaanskii/vansify/db"
+)
+
+func VerifyEmail(c *gin.Context){
+	token := c.Query("token")
+	parts := strings.Split(token, ":")
+	if len(parts) != 2 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	email := parts[1]
+	// Find the user by email and update the verified status
+	_, err := db.DB.Exec("UPDATE users SET verified = ? WHERE email =?", true, email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error verifying email"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Email verified successfully! You can now log in."})
+}
