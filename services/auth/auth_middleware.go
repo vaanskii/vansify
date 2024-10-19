ef87a57ee53ea22ab7439d3ab85707520fe39a74
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-10-20 03:44:27 +0400

    Integrate authmiddleware. - Implemented a AuthMiddleware function which handling authorization.

---
+++ services/auth/auth_middleware.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vaanskii/vansify/utils"
+)
+
+func AuthMiddleware() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		var token string
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer"){
+			token = strings.TrimPrefix(authHeader, "Bearer")
+		}
+
+		if token == "" {
+			token = c.Query("token")
+		}
+
+		if token == "" {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+            return
+        }
+		
+		_, err := utils.VerifyJWT(token)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+		c.Next()
+	}
+}
