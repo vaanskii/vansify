46a5447979f7a81b19d54412d774a3e2c4dfa7c6
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-10-20 03:43:32 +0400

    integrate chat models. - Integrate Message and Chat structs which will be used for chat.

---
+++ models/chat_model.go
@@ -0,0 +1,16 @@
+package models
+
+type Message struct {
+	ID 		  int 	   `json:"id"`
+	ChatID    string   `json:"chat_id"`
+	Message   string   `json:"text"`
+	Username  string   `json:"username"`
+}
+
+type Chat struct {
+	ID        int      `json:"id"`
+	ChatID    int      `json:"chat_id"`
+	User1  string 	   `json:"user1"`
+    User2  string      `json:"user2"`
+}
+
