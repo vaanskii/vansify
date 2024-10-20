dfa69176c836e6ae3fe16cfbc7476ee95edef368
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-10-20 20:56:34 +0400

    Update AuthMiddleware: Add claims extraction and verification in context.

--- services/auth/auth_middleware.go
+++ services/auth/auth_middleware.go
@@ -8,30 +8,36 @@ import (
 	"github.com/vaanskii/vansify/utils"
 )
 
-func AuthMiddleware() gin.HandlerFunc{
-	return func(c *gin.Context) {
-		var token string
+func AuthMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        var token string
+        authHeader := c.GetHeader("Authorization")
+        if authHeader != "" && strings.HasPrefix(authHeader, "Bearer ") {
+            token = strings.TrimPrefix(authHeader, "Bearer ")
+        }
 
-		authHeader := c.GetHeader("Authorization")
-		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer"){
-			token = strings.TrimPrefix(authHeader, "Bearer")
-		}
+        if token == "" {
+            token = c.Query("token")
+        }
 
-		if token == "" {
-			token = c.Query("token")
-		}
-
-		if token == "" {
+        if token == "" {
             c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
             return
         }
-		
-		claims, err := utils.VerifyJWT(token)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-			return
-		}
-		c.Set("claims", claims)
-		c.Next()
-	}
+
+        parsedToken, err := utils.VerifyJWT(token)
+        if err != nil {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+            return
+        }
+
+        claims, ok := parsedToken.Claims.(*utils.CustomClaims)
+        if !ok || !parsedToken.Valid {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+            return
+        }
+
+        c.Set("claims", claims)
+        c.Next()
+    }
 }
