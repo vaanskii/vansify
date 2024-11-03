4db39e66c38116a02fe94e5586edde45331e8d4f
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-11-04 01:41:15 +0400

    Use PORT environment variable for dynamic port binding

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -16,11 +17,11 @@ import (
 )
 
 func main() {
-	db.ConnectToDatabase()
+    db.ConnectToDatabase()
 
-	r := gin.Default()
+    r := gin.Default()
 
-	// Enable global handling of Method Not Allowed
+    // Enable global handling of Method Not Allowed
     r.HandleMethodNotAllowed = true
 
     // Handle non-existent and incorrect methods routes
@@ -30,61 +31,64 @@ func main() {
     r.NoMethod(func(c *gin.Context) {
         c.JSON(http.StatusMethodNotAllowed, gin.H{"error": "Method Not Allowed"})
     })
-	
-	r.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 
-	r.Static("/assets", "./assets")
+    r.Use(cors.New(cors.Config{
+        AllowAllOrigins:  true,
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
 
-	v1 := r.Group("/v1")
-	{
-		// Authorization Routes
-		v1.POST("/register", auth.RegisterUser)
-		v1.POST("/login", auth.LoginUser)
-		v1.GET("/verify", auth.VerifyEmail)
-		v1.DELETE("/delete-account", auth.AuthMiddleware(), auth.DeleteUser)
-		v1.POST("/forgot-password", auth.ForgotPassword)
-		v1.POST("/reset-password", auth.ResetPassword)
+    r.Static("/assets", "./assets")
 
-		// refresh token 
-		v1.POST("/refresh-token", utils.RefreshToken)
+    v1 := r.Group("/v1")
+    {
+        // Authorization Routes
+        v1.POST("/register", auth.RegisterUser)
+        v1.POST("/login", auth.LoginUser)
+        v1.GET("/verify", auth.VerifyEmail)
+        v1.DELETE("/delete-account", auth.AuthMiddleware(), auth.DeleteUser)
+        v1.POST("/forgot-password", auth.ForgotPassword)
+        v1.POST("/reset-password", auth.ResetPassword)
 
-		// Follow/Unfollow system Routes
-		v1.POST("/follow/:username", auth.AuthMiddleware(), follow.FollowUser)
-		v1.DELETE("/unfollow/:username", auth.AuthMiddleware(),follow.UnfollowUser)
-		v1.GET("/is-following/:follower/:following", auth.AuthMiddleware(), follow.CheckFollowStatus)
-		v1.GET("/followers/:username", auth.AuthMiddleware(), follow.GetFollowers)
-		v1.GET("/following/:username", auth.AuthMiddleware(), follow.GetFollowing)
-		v1.GET("/notifications/ws", auth.AuthMiddleware(), chat_notifications.NotificationWsHandler)
+        // refresh token
+        v1.POST("/refresh-token", utils.RefreshToken)
 
+        // Follow/Unfollow system Routes
+        v1.POST("/follow/:username", auth.AuthMiddleware(), follow.FollowUser)
+        v1.DELETE("/unfollow/:username", auth.AuthMiddleware(), follow.UnfollowUser)
+        v1.GET("/is-following/:follower/:following", auth.AuthMiddleware(), follow.CheckFollowStatus)
+        v1.GET("/followers/:username", auth.AuthMiddleware(), follow.GetFollowers)
+        v1.GET("/following/:username", auth.AuthMiddleware(), follow.GetFollowing)
+        v1.GET("/notifications/ws", auth.AuthMiddleware(), chat_notifications.NotificationWsHandler)
 
-		// Chat routes
-		v1.POST("/create-chat", auth.AuthMiddleware(), chat.CreateChat)
-		v1.GET("/chat/:chatID", auth.AuthMiddleware(), chat.WsHandler)
-		v1.GET("/chat/:chatID/history", auth.AuthMiddleware(), chat.GetChatHistory)
-		v1.GET("/check-chat/:user1/:user2", auth.AuthMiddleware(), chat.CheckChatExists)
-		v1.GET("/notifications/unread", auth.AuthMiddleware(), chat_notifications.GetUnreadNotifications)
-		v1.POST("/notifications/mark-read/:chatID", auth.AuthMiddleware(), chat_notifications.MarkChatNotificationsAsRead)
-		v1.DELETE("/chat/:chatID", auth.AuthMiddleware(), chat.DeleteChat)
-		v1.DELETE("/message/:messageID", auth.AuthMiddleware(), chat.DeleteMessage)
+        // Chat routes
+        v1.POST("/create-chat", auth.AuthMiddleware(), chat.CreateChat)
+        v1.GET("/chat/:chatID", auth.AuthMiddleware(), chat.WsHandler)
+        v1.GET("/chat/:chatID/history", auth.AuthMiddleware(), chat.GetChatHistory)
+        v1.GET("/check-chat/:user1/:user2", auth.AuthMiddleware(), chat.CheckChatExists)
+        v1.GET("/notifications/unread", auth.AuthMiddleware(), chat_notifications.GetUnreadNotifications)
+        v1.POST("/notifications/mark-read/:chatID", auth.AuthMiddleware(), chat_notifications.MarkChatNotificationsAsRead)
+        v1.DELETE("/chat/:chatID", auth.AuthMiddleware(), chat.DeleteChat)
+        v1.DELETE("/message/:messageID", auth.AuthMiddleware(), chat.DeleteMessage)
 
-		
-		// User Profile Retrieval
-		v1.GET("/me/chats", auth.AuthMiddleware(), user.GetUserChats)
-		v1.GET("/user/:username", user.GetUserByUsername)
-	}
+        // User Profile Retrieval
+        v1.GET("/me/chats", auth.AuthMiddleware(), user.GetUserChats)
+        v1.GET("/user/:username", user.GetUserByUsername)
+    }
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "CORS-enabled route!",
-		})
-	})
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "CORS-enabled route!",
+        })
+    })
 
-	r.Run(":8080")
+    // Use the PORT environment variable if available, otherwise default to 8080
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    r.Run(":" + port)
 }
