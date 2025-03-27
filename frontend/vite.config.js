794fe0e8f8cb736f6f8155d6092d1588d5befa26
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-03-27 18:53:05 +0400

    commit vite.config.js and package jsons .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      tailwindcss(),
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
