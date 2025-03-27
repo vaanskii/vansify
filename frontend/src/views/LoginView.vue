8736a64a752c7e8c19e7e6f4f3c877711d629f7b
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-03-27 18:16:16 +0400

    add new route link to loginview.vue which will redirect to signup page.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,19 +2,19 @@
   <div class="login-container flex flex-col gap-3 max-w-[26rem] w-full mx-auto px-10 py-10">
     <form @submit.prevent="login">
       <div class="relative z-0">
-          <input type="text" id="username" v-model="username" autocomplete="username"  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+          <input required type="text" id="username" v-model="username" autocomplete="username"  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
           <label for="username" class="absolute text-sm text-gray-500 dark:text-gray-[#757575] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Username</label>
       </div>
       <div class="relative z-0 mt-5">
-        <input type="password" id="password" v-model="password" autocomplete="current-password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+        <input required type="password" id="password" v-model="password" autocomplete="current-password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
         <label for="password" class="absolute text-sm text-gray-500 dark:text-gray-[#757575] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password</label>
       </div>
-      <div class="flex items-center justify-between mt-4 sm:flex-row flex-col sm:gap-0 gap-2">
-        <label class="flex items-center space-x-2 text-[#757575]">
-          <input class="text-sm" type="checkbox" v-model="rememberMe">
-          <span>Remember Me</span>
+      <div class="flex items-center justify-between mt-4 xs:flex-row gap-2 flex-col">
+        <label class="flex items-center space-x-2 text-sm sm:text-base text-[#757575]">
+          <input class="text-sm sm:text-base" type="checkbox" v-model="rememberMe">
+          <span class="text-sm sm:text-base">Remember Me</span>
         </label>
-        <router-link class="text-[#757575] sm:mt-0 mt-2" to="/forgot-password">Forgot Password?</router-link>
+        <router-link class="text-sm sm:text-base text-[#757575]" to="/forgot-password">Forgot Password?</router-link>
       </div>
       <button class="w-full mt-6 shadow shadow-hover-sm cursor-pointer h-11 rounded-[3px] text-[14px] font-medium text-[#757575]" type="submit">
         <span v-if="isLoading"> 
@@ -25,7 +25,7 @@
         <span v-else>Login</span>
       </button> 
     </form>
-    <button @click="loginWithGoogle" type="button" class="login-with-google-btn mb-1" >Sign in with Google</button>
+    <button @click="loginWithGoogle" type="button" class="login-with-google-btn" >Sign in with Google</button>
     <div v-if="router.currentRoute.value.path === '/'">
       <div class="flex items-center my-4">
       <div class="flex-grow border-t border-gray-300"></div>
@@ -33,9 +33,13 @@
       <div class="flex-grow border-t border-gray-300"></div>
     </div>
 
-    <router-link to="/signup" class="w-full mt-2 shadow shadow-hover-md bg-[#757575] hover:bg-[#707070] cursor-pointer h-11 rounded-[3px] text-[14px] font-medium text-white flex items-center justify-center">
+    <router-link to="/signup" class="w-full mt-6 shadow shadow-hover-md bg-[#757575] hover:bg-[#707070] cursor-pointer h-11 rounded-[3px] text-[14px] font-medium text-white flex items-center justify-center">
         Create New Account
     </router-link>
+    </div>
+    <div v-else-if="router.currentRoute.value.path === '/login'" class="text-center">
+      <span class="text-[#757575]">• </span>
+      <router-link to="/signup" class="text-[#757575] text-sm underline">Sign up for Vansify</router-link>
     </div>
     <div v-if="error" class="error">{{ error }}</div>
     <div v-if="message" class="message">{{ message }}</div>
