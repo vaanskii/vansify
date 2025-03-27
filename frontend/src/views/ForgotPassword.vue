325dda8b5050a172367dfadd355dc8b61e7cefb2
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-03-28 01:48:22 +0400

    modify forgotpassword.vue and resetpassword.vue for better styling.

--- frontend/src/views/ForgotPassword.vue
+++ frontend/src/views/ForgotPassword.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="forgot-password">
-        <h1>Forgot Password</h1>
+    <div class="forgot-password flex flex-col gap-3 max-w-[26rem] w-full mx-auto px-10 py-10">
+        <h1 class="text-2xl uppercase text-[#757575]">Find your account</h1>
+        <p class="text-[#757575]">Please enter your email to search for your account.</p>
+        <div class="flex items-center my-4">
+            <p class="flex-grow border-t border-gray-300"></p>
+        </div>
         <form @submit.prevent="handleSubmit">
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" v-model="email" required />
+            <div class="relative z-0 mt-5">
+                <input required type="email" id="email" v-model="email" autocomplete="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label for="email" class="absolute text-sm text-gray-500 dark:text-gray-[#757575] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email</label>
             </div>
-            <button type="submit">Submit</button>
+            <button class="w-full mt-6 shadow shadow-hover-sm cursor-pointer h-11 rounded-[3px] text-[14px] font-medium text-[#757575]" type="submit">
+                <span v-if="isLoading"> 
+                <div class="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-[#757575] rounded-full" role="status" aria-label="loading">
+                    <span class="sr-only">Loading...</span>
+                </div>
+                </span>
+                <span v-else>Submit</span>
+            </button> 
         </form>
         <p v-if="message">{{ message }}</p>
     </div>
@@ -18,9 +29,11 @@ import axios from 'axios';
 
 const email = ref('');
 const message = ref('');
+const isLoading = ref(false);
 
 const handleSubmit = async () => {
     try {
+        isLoading.value = true
         const response = await axios.post('/v1/forgot-password', { email: email.value });
         message.value = response.data.message;
     } catch (error) {
@@ -29,54 +42,18 @@ const handleSubmit = async () => {
         } else {
             message.value = 'An error occurred. Please try again.';
         }
+    } finally {
+        isLoading.value = false
     }
 };
 </script>
 
 <style scoped>
 .forgot-password {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-
-.forgot-password h1 {
-    text-align: center;
-}
-
-.forgot-password form {
-    display: flex;
-    flex-direction: column;
-}
-
-.forgot-password label {
-    margin-bottom: 5px;
-}
-
-.forgot-password input {
-    margin-bottom: 10px;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-
-.forgot-password button {
-    padding: 10px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-.forgot-password button:hover {
-    background-color: #0056b3;
-}
-
-.forgot-password p {
-    text-align: center;
-    color: green;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    gap: 1;
 }
 </style>
--- frontend/src/views/ResetPassword.vue
+++ frontend/src/views/ResetPassword.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="reset-password">
-    <h1>Reset Password</h1>
+  <div class="reset-password flex flex-col gap-3 max-w-[26rem] w-full mx-auto px-10 py-10">
+    <h1 class="text-2xl uppercase text-[#757575]">Reset Password</h1>
+    <div class="flex items-center my-4">
+        <p class="flex-grow border-t border-gray-300"></p>
+    </div>
     <form v-if="!passwordChanged" @submit.prevent="resetPassword">
-      <div>
-        <label for="newPassword">New Password:</label>
-        <input type="password" v-model="newPassword" required />
+      <div class="relative z-0 mt-5">
+          <input required type="password" id="newPassword" v-model="newPassword" autocomplete="password"  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+          <label for="newPassword" class="absolute text-sm text-gray-500 dark:text-gray-[#757575] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">New password</label>
       </div>
-      <div>
-        <label for="confirmPassword">Confirm Password:</label>
-        <input type="password" v-model="confirmPassword" required />
+      <div class="relative z-0 mt-5">
+          <input required type="password" id="confirmPassword" v-model="confirmPassword" autocomplete="password"  class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+          <label for="confirmPassword" class="absolute text-sm text-gray-500 dark:text-gray-[#757575] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Confirm Password</label>
       </div>
-      <button type="submit">Reset Password</button>
+      <button class="w-full mt-6 shadow shadow-hover-sm cursor-pointer h-11 rounded-[3px] text-[14px] font-medium text-[#757575]" type="submit">
+        <span v-if="isLoading"> 
+          <div class="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-[#757575] rounded-full" role="status" aria-label="loading">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </span>
+        <span v-else>Reset password</span>
+      </button> 
     </form>
     <p v-if="message">{{ message }}</p>
   </div>
@@ -25,7 +35,8 @@ const newPassword = ref('');
 const confirmPassword = ref('');
 const message = ref('');
 const passwordChanged = ref(false);
-const router = useRouter(); // Use Vue Router
+const router = useRouter();
+const isLoading = ref(false);
 
 const resetPassword = async () => {
   if (newPassword.value !== confirmPassword.value) {
@@ -43,6 +54,7 @@ const resetPassword = async () => {
   }
 
   try {
+    isLoading.value = true;
     const response = await axios.post('/v1/reset-password', {
       token: token,
       new_password: newPassword.value,
@@ -58,50 +70,18 @@ const resetPassword = async () => {
   } catch (error) {
     const errorMsg = error.response ? error.response.data.error : 'An error occurred';
     message.value = errorMsg;
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
 
 <style scoped>
 .reset-password {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 1em;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-.reset-password h1 {
-  text-align: center;
-}
-.reset-password form {
-  display: flex;
-  flex-direction: column;
-}
-.reset-password div {
-  margin-bottom: 1em;
-}
-.reset-password label {
-  margin-bottom: 0.5em;
-  font-weight: bold;
-}
-.reset-password input {
-  padding: 0.5em;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-.reset-password button {
-  padding: 0.5em;
-  border: none;
-  border-radius: 4px;
-  background-color: #007bff;
-  color: white;
-  cursor: pointer;
-}
-.reset-password button:hover {
-  background-color: #0056b3;
-}
-.reset-password p {
-  text-align: center;
-  color: red;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  gap: 1;
 }
 </style>
