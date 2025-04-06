e3346e475cc74e2ac8e3f27349ece67ebf052629
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-04-07 02:08:54 +0400

    Integrate database.

--- backend/db/db.go
+++ backend/db/db.go
@@ -14,34 +14,34 @@ var DB *sql.DB
 
 // ConnectToDatabase initializes the database connection
 func ConnectToDatabase() {
-    // Conditionally load environment variables from .env file if it exists
-    if _, err := os.Stat(".env"); err == nil {
-        if loadErr := godotenv.Load(); loadErr != nil {
-            log.Fatalf("Error loading .env file: %v", loadErr)
-        }
-    }
+	// Conditionally load environment variables from .env file if it exists
+	if _, err := os.Stat(".env"); err == nil {
+		if loadErr := godotenv.Load(); loadErr != nil {
+			log.Fatalf("Error loading .env file: %v", loadErr)
+		}
+	}
 
-    // Get environment variables
-    dbUser := os.Getenv("DB_USER")
-    dbPass := os.Getenv("DB_PASS")
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbName := os.Getenv("DB_NAME")
+	// Get environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
 
-    // Remove local DB variable declaration
-    var err error // Declare err here
-    DB, err = sql.Open("mysql", dsn)
-    if err != nil {
-        log.Fatalf("Error opening database: %v", err)
-    }
+	// Remove local DB variable declaration
+	var err error // Declare err here
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
 
-    // Verify the connection
-    err = DB.Ping()
-    if err != nil {
-        log.Fatalf("Error connecting to the database: %v", err)
-    }
+	// Verify the connection
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
 
-    fmt.Println("Successfully connected to MySQL database!")
+	fmt.Println("Successfully connected to MySQL database!")
 }
